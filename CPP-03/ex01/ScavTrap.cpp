0e2ce329d0eb28148b4e1f200967df4c41c6c8c8
ychun816 <good62226@gmail.com>
2025-04-05 18:21:26 +0200

    oops forgot scavtrp copy and assign construct, added

--- CPP-03/ex01/ScavTrap.cpp
+++ CPP-03/ex01/ScavTrap.cpp
@@ -3,13 +3,38 @@
 //For inheritance, the derived class (ScavTrap) inherits the members of the base class (ClapTrap)
 //calling the base class constructor is generally considered a better practice according to common C++ conventions
 
-
 //constructs & destructors
 ScavTrap::ScavTrap() : ClapTrap()  // Call to base class default constructor
 {
     _hitPoints = 100;
     _energyPoints = 50;
     _attackDamage = 20;
+}
+
+//copy
+ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other)
+{
+    ClapTrap::operator=(other); // Use ClapTrap's assignment operator
+    _name = other._name;
+    _hitPoints = other._hitPoints;
+    _energyPoints = other._energyPoints;
+    _attackDamage = other._attackDamage;
+    std::cout << "ClapTrap " << _name << "is copied!" << std::endl;
+}
+
+//assign
+ScavTrap& ScavTrap::operator=(const ScavTrap& other)
+{
+    if (this != &other)
+    {
+        ClapTrap::operator=(other); // Use ClapTrap's assignment operator
+        _name = other._name;
+        _hitPoints = other._hitPoints;
+        _energyPoints = other._energyPoints;
+        _attackDamage = other._attackDamage;
+    }
+    std::cout << "ScavTrap " << _name << "is assigned!" << std::endl;
+    return (*this);
 }
 
 ScavTrap::ScavTrap(std::string name) : ClapTrap(name) // Call to base class constructor with name
