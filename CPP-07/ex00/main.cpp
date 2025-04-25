1d167adefd690f95684e237049504fa73e74f338
ychun816 <good62226@gmail.com>
2025-04-25 15:10:54 +0200

    checked ex00

--- CPP-07/ex00/main.cpp
+++ CPP-07/ex00/main.cpp
@@ -1,6 +1,5 @@
 #include "whatever.hpp"
 
-
 int main()
 {
     int a = 2;
@@ -9,7 +8,8 @@ int main()
     std::cout << "=== CREATE INT ===" << std::endl;
     std::cout << "a: " << a << " | b: " << b << std::endl;
 
-    std::cout << "\n=== TEST SWAP ===" << std::endl;
+    std::cout << "\n⊹ ࣪ ﹏𓊝﹏𓂁﹏⊹ ࣪ ˖" << std::endl;
+    std::cout << "=== TEST SWAP ===" << std::endl;
     std::cout << "-- Before swap --" << std::endl; 
     std::cout << "a = " << a << " | b = " << b << std::endl;
     swap(a, b);
@@ -17,9 +17,11 @@ int main()
     std::cout << "-- After swap --" << std::endl;
     std::cout << "a = " << a << " | b = " << b << std::endl;
 
-    std::cout << "\n=== TEST MIN ===" << std::endl;
+    std::cout << "\n⊹ ࣪ ﹏𓊝﹏𓂁﹏⊹ ࣪ ˖" << std::endl;
+    std::cout << "=== TEST MIN ===" << std::endl;
     std::cout << "Min: " << min(a, b) << std::endl;
 
-    std::cout << "\n=== TEST MAX ===" << std::endl;
+    std::cout << "\n⊹ ࣪ ﹏𓊝﹏𓂁﹏⊹ ࣪ ˖" << std::endl;
+    std::cout << "=== TEST MAX ===" << std::endl;
     std::cout << "Max: " << max(a, b) << std::endl;
 }
--- CPP-07/ex00/whatever.hpp
+++ CPP-07/ex00/whatever.hpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 
-//swap
 template <typename T>
 void swap(T &a, T &b)
 {
@@ -30,19 +29,21 @@ const T& max(const T& a, const T& b)
 #endif 
 
 
-/* NOTES
-define a template function
-every template function must be preceded by its own template <typename T> declaration!
-Each function must be prefixed with 'the template'
-If working with template classes -> can write the template <typename T> just once before the class definition, and all methods inside will use the same type.
+/* NOTES:
+//define a template function
+- Every template function must be preceded by its own template <typename T> declaration!
+- Each function must be prefixed with 'the template'
+- If working with template classes -> can write the template <typename T> just once before the class definition, and all methods inside will use the same type
 
-template<...> tells the compiler: “I’m about to define a template function or class.”
-typename T means: “T is a placeholder for a data type (like int, float, string, etc).”
-T is not a real type — it's a symbol you define
-When you call the function, the compiler figures out what T should be based on the argument types.
-!!! Using const T& avoids unnecessary copying (If T is a large object, copying it directly is slow and wasteful)
+//template<...> : tell compiler: “I’m about to define a template function or class”
 
-Q: Why use references (&)?
-T &a means pass by reference, not by value.
-If you just used T a, it would make a copy, and any change to a would only affect the copy.
+//typename T: “T is a placeholder for a data type (like int, float, string, etc)”
+- T is not a real type -> it's a symbol you define
+- When call the function, the compiler figures out what T should be based on the argument types
+- !!! Using const T& avoids unnecessary copying (If T is a large object, copying it directly is slow and wasteful)
+
+//Why use references (&)?
+- T &a means pass by reference, not by value.
+- If just used T a -> it makes a copy -> any change to 'a' would only affect the copy!
+
 */
