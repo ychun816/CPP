602ad3aa543ae1c805376e5ef68257339af8834b
ychun816 <good62226@gmail.com>
2025-04-25 15:32:28 +0200

    checked ex2, added multiply operator

--- CPP-07/ex01/Iter.hpp
+++ CPP-07/ex01/Iter.hpp
@@ -4,17 +4,9 @@
 #include <iostream>
 #include <string>
 
-//pass a function as a parameter to another function
-//pass template functions to another template
-
-//IMPLEMENT STEPS:
-//Implement iter() as a function template.
-//Create a print() function template.
-//Test with arrays of: int / float / std::string
-
 //iter
 template <typename T, typename Function>
-void iter(T *array, int len, Function func)//(T &)
+void iter(T *array, int len, Function func)
 {
     if (len <= 0)
         return ;
@@ -25,43 +17,48 @@ void iter(T *array, int len, Function func)//(T &)
 }
 
 //print
-// print() is a function template!! not "a function" that takes a reference to a variable of type T
+//print() is a function template!! not "a function" that takes a reference to a variable of type T
 //value is a reference to a variable of type T
-//You’re not copying the value, and you can modify it if needed
-// use references unless you specifically need pointers -> 
+//not copy the value, can modify it if needed
 template <typename T>
 void print(const T &value)
 {
     std::cout << value << std::endl;
 }
 
-//checkeven
+//check even
 template <typename T>
 void checkEven(const T &value)
 {
     (value % 2 == 0) ? std::cout << value << " is even\n" : std::cout << value << " is odd\n";
 }
 
-//multiply
+//multiply *2
 template <typename T>
 void multiplyTWO(const T &value)
 {
     std::cout << value * 2 << std::endl;
 }
 
-// // Multiply functor template
-// template <typename T>
-// struct Multiply 
-//{
-//     int factor;
-//     Multiply(int factor) : factor(factor) {}
-//     void operator()(T &element) {
-//         element *= factor;  // Multiply the element by the factor
-//     }
-// };
+/*
+[ref] Multiply factor func*/
+template <typename T>
+struct Multiply 
+{
+    int factor;
+
+    Multiply(int factor) : factor(factor) {}
+
+    void operator()(T &element)
+    {
+        element *= factor;
+    }
+};
+
 
 
 #endif 
+
 
 /* NOTES
 
@@ -77,17 +74,15 @@ Reusability	: Separate logic into .hpp, test in main.cpp, and compile via Makefi
 
 Function template as arg : Use a templated function like print<T>() as a callback
 
-*/
 
 
-/*
+//Reference v.s. Pointer
 
 Concept     | Analogy                        | Code
 ---------------------------------------------------
 Reference   | A nickname                     | T &x
 Pointer     | A phone number                 | T *x
 Dereference | Call the phone (get the value) | *x
-
 
 T *array          :	Pointer to first element of array of type T
 T &x	          : Reference to a T (no copy, can modify original)
--- CPP-07/ex01/main.cpp
+++ CPP-07/ex01/main.cpp
@@ -1,27 +1,31 @@
 #include "Iter.hpp"
 
+// Explicitly instantiate print<int> for use with iter)
+// in C++98 need to specify it -> This is not necessary in C++11 and later, as templates are instantiated automatically (with 'auto')
+
 int main()
 {
-    std::cout << "=== Test print int ===" << std::endl;
+    std::cout << "=== Print int ===" << std::endl;
     int nbs[] = {1, 2, 3};
 
-    // Explicitly instantiate print<int> for use with iter
-    // in C++98 need to specify it -> This is not necessary in C++11 and later, as templates are instantiated automatically
     iter(nbs, 3, print<int>);
 
-    std::cout << "\n=== Test print float ===" << std::endl;
+    std::cout << "\n=== Print float ===" << std::endl;
     float floats[] = {1.1f, 2.2f, 3.3f, 42.4f};
     iter(floats, 4, print<float>);
 
-    std::cout << "\n=== Test check even (int) ===" << std::endl;
+    std::cout << "\n=== Check even (int) ===" << std::endl;
     int nbs2[] = {1, 2, 3, 4};
     iter(nbs2, 4, checkEven<int>);
 
-    std::cout << "\n=== Test multiplyTWO (int) ===" << std::endl;
+    std::cout << "\n=== multiplyTWO (int) ===" << std::endl;
     int nbs3[] = {1, 2, 3, 4, 100};
     iter(nbs3, 5, multiplyTWO<int>);
 
-    //std::cout << "\n=== Test multiply(int) ===" << std::endl;
+    std::cout << "\n=== Multiply operator (int) ===" << std::endl;
+    int nbs4[] = {1, 2, 3, 5, 7};
+    iter(nbs4, 5, Multiply<int>(5));
+    iter(nbs4, 5, print<int>);
 
     return 0;
 }
